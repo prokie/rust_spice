32178f6155479a49304025f310462cad11d8537e
Pontus <pontus.thomsson@ericsson.com>
2023-06-12 21:06:01 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,55 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#![allow(dead_code)]
+
+use nom::IResult;
+#[derive(Debug, PartialEq)]
+pub enum ComponentType {
+    VoltageSource,
+    CurrentSource,
+    Resistor,
+    Capacitor,
+    Inductor,
+    Diode,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Component {
+    pub node_1: String,
+    pub node_2: String,
+    pub parameters: String,
+    pub _type: ComponentType,
+    pub name: String,
+}
+
+fn components(input: &str) -> IResult<&str, Component> {
+    let components = Component {
+        _type: ComponentType::VoltageSource,
+        node_1: "in".to_string(),
+        node_2: "0".to_string(),
+        parameters: "1".to_string(),
+        name: "1".to_string(),
+    };
+
+    Ok(("", components))
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use libloading::library_filename;
-    use std::ffi::{c_void, CStr, CString};
-    use std::os::raw::{c_char, c_int};
-
-    extern "C" fn controlled_exit(
-        _arg1: c_int,
-        _arg2: bool,
-        _arg3: bool,
-        _arg4: c_int,
-        _arg5: *mut c_void,
-    ) -> c_int {
-        return 0;
-    }
-
-    static mut responses: Vec<String> = Vec::new();
-
-    unsafe extern "C" fn send_char(arg1: *mut c_char, _arg2: c_int, _arg3: *mut c_void) -> c_int {
-        let s = CStr::from_ptr(arg1)
-            .to_str()
-            .expect("could not make string");
-        println!("{}", s);
-        responses.push(s.to_string());
-        return 0;
-    }
 
     #[test]
-    fn init_works() {
-        unsafe {
-            let ng = ngspice::new(library_filename("ngspice")).unwrap();
-            ng.ngSpice_Init(
-                Some(send_char),
-                None,
-                Some(controlled_exit),
-                None,
-                None,
-                None,
-                std::ptr::null_mut(),
-            );
-            let s = CString::new(
-                "source C:/Users/pontu/Documents/rust_spice/tests/circuits/voltage_divider.sp",
-            )
-            .expect("CString::new failed");
-            ng.ngSpice_Command(s.into_raw());
-            let s = CString::new("op").expect("CString::new failed");
-            ng.ngSpice_Command(s.into_raw());
-            let s = CString::new("print out").expect("CString::new failed");
-            ng.ngSpice_Command(s.into_raw());
-            assert_eq!(
-                responses.last().unwrap_or(&String::new()),
-                "stdout out = 6.666667e-01"
-            );
-        }
+    fn test_components() {
+        assert_eq!(
+            components("V1 in 0 1"),
+            Ok((
+                "",
+                Component {
+                    _type: ComponentType::VoltageSource,
+                    node_1: "in".to_string(),
+                    node_2: "0".to_string(),
+                    parameters: "1".to_string(),
+                    name: "1".to_string(),
+                }
+            ))
+        );
     }
 }
